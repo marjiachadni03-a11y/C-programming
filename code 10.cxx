4b6cd1334cdfb0a2470b9bba3b3888446424890c
Marjia akter chadni <marjiachadni03@gmail.com>
2026-06-19 11:40:59 -0700

    Delete code 10.cxx

--- code 10.cxx
+++
@@ -1,9 +0,0 @@
-include <stdio.h>
-int main() {
-    int num, square;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    square = num * num;
-    printf("The square of %d is %d.\n", num, square);
-    return 0;
-}
