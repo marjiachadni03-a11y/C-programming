0d642286daa688f102024610fecfda59d3df4b92
Marjia akter chadni <marjiachadni03@gmail.com>
2026-06-19 11:42:29 -0700

    Delete code 9.cxx

--- code 9.cxx
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int number, result;
-    printf("Enter any number: ");
-    scanf("%d", &number);
-    result = number * 2;
-    printf("Double of %d is %d.\n", number, result);
-    return 0;
-}}
