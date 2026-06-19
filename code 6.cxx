a26e1f340d2c26c27a4b0869e566ae6cc9695eb5
Marjia akter chadni <marjiachadni03@gmail.com>
2026-06-19 11:42:11 -0700

    Delete code 6.cxx

--- code 6.cxx
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int currentYear, nextYear;
-    printf("Enter the current year: ");
-    scanf("%d", &currentYear);
-    nextYear = currentYear + 1;
-    printf("Next year will be %d.\n", nextYear);
-    return 0;
-}
