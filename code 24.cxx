302fb8d4d5eae75d294b530ae19f561355539c18
Marjia akter chadni <marjiachadni03@gmail.com>
2026-06-19 11:41:44 -0700

    Delete code 24.cxx

--- code 24.cxx
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float meters, centimeters;
-    printf("Enter height in meters (e.g., 1.75): ");
-    scanf("%f", &meters);
-    centimeters = meters * 100.0;
-    printf("%f meters is exactly %f centimeters.\n", meters, centimeters);
-    return 0;
-}
