70158e2b3976a186c92562ab72150eadbc4e176b
Marjia akter chadni <marjiachadni03@gmail.com>
2026-06-24 23:48:41 -0700

    Add files via upload

---
+++ post decrement.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    x--; // x becomes 4
+    printf("%d", x); // Prints: 4
+    return 0;
+}
---
+++ post increment.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    x++; // x becomes 6
+    printf("%d", x); // Prints: 6
+    return 0;
+}
